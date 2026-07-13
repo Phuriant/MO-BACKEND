1e0d64b9effcbf3bfab33cd7b2d4389b6e96beb1
phurinat siritham <phurinat.siritham@gmail.com>
2026-07-13 14:49:10 +0700

    Initial commit

--- libs/date_utils.js
+++ libs/date_utils.js
@@ -1,12 +1,12 @@
-module.exports = {
-  getCurrentDateForToken: () => {
-    const now = new Date();
-    const formattedDate = new Intl.DateTimeFormat('en-GB', {
-      day: '2-digit',
-      month: '2-digit',
-      year: 'numeric'
-    }).format(now).replace(/\//g, '-');
+module.exports = { //สั่ง export object นี้ออกไปเพื่อไห้้ไฟล์ js อื่นๆเห็นเเละนำไปใข้งานได้
+  getCurrentDateForToken: () => { //ประกาศชื่อฟังก์ชั่นเเบบ arrow
+    const now = new Date(); //ประกาศตัวเเปรชื่อ now สร้าง object ของวันเดือนปี current
+    const formattedDate = new Intl.DateTimeFormat('en-GB', { //เครื่องมือมาตราฐานของjs เพื่อจัด formatของวันที่ตามเวลาเเละท้องถิ่น Engb คือ รหัสภาษาของอังกฤษ ใ่ช้ตัวนี้เพราะเป็นมาตราฐาน
+      day: '2-digit', // เซ็ตไห้วันที่เเบบเลข2หลัก 
+      month: '2-digit', // เซ็ตไห้เดือนเป็น 2 หลัก
+      year: 'numeric' // เซ็ตไห้ปีเป็นเเบบปกติ
+    }).format(now).replace(/\//g, '-'); // นำเวลาปัจจุบันของ now มาใส่format ไว้ผลลัพจะได้ 7/13/26  replace จะสั่งไห้เปลี่ยนเครื่องหมายเเสลชเป็น -
 
-    return formattedDate;
+    return formattedDate; //ส่งค่าผลลัพธ์ ที่อยู่ในตัวเเปร formattedDate
   }
 }
--- libs/db_pool.js
+++ libs/db_pool.js
@@ -1,11 +1,11 @@
-const mariadb = require('mariadb');
-const pool = mariadb.createPool({
+const mariadb = require('mariadb');//เป็นการดึงLibrary ของmariadb เข้ามาใช้งานในไฟล์นี
+const pool = mariadb.createPool({ //ฟังชั่นที่สั่งไห้ระบบสร้างกลุ่มของการเชื่อมต่อรอไว้ พอมีคนจะใช้ก้มาหยิบไปใช้เสร็จก็คืน
     host: 'localhost',
-    user: 'root',
-    password: '1234',
-    port: 3307,
-    database: 'my_fitness', 
-    connectionLimit: 5
+    user: 'root', //username mariadb
+    password: '1111', //password maiadb
+    port: 3306, //port mairadb
+    database: 'my_fitness', //folder myfitness
+    connectionLimit: 5 //จำกัดจำนวนการเชื่อมต่อสูงสุด
 });
 
-module.exports = pool;
+module.exports = pool; //การส่งออกตัวเเปร pool โดยไม่ต้องเขียนโค้ดตั้งค่าไหม่ซ้ำๆเมื่อไฟล์อื่นต้องการใช้งาน
